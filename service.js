75d99492d18379b7d8491ea3a7c9704e7504f136
Jonathan Grunder <jonathan.grunder@comdatagroup.fr>
2020-05-05 16:09:51 +0200

    Create global _servers
    Available server is now removed from the global _servers array and we stop the application only if there is no more server to monitor

--- service.js
+++ service.js
@@ -6,15 +6,17 @@ const AVAIL_URL = 'https://www.ovh.com/engine/api/dedicated/server/availabilitie
 
 console.log('New instance of Kimsufi Alert');
 
-console.log(config.servers);
+var _servers = config.servers;
 
-var interval = setInterval(isAvailable, 10000, config.servers);
+console.log(_servers);
+
+var interval = setInterval(isAvailable, 10000, _servers);
 
 function isAvailable(servers)
 {
-  for(var data in config.servers)
+  for(var data in _servers)
   {
-    var server = config.servers[data];
+    var server = _servers[data];
     console.log('Is server "' + server + '" available?');
     request(AVAIL_URL + server, function (error, response, body) {
       var jsonContent = JSON.parse(body);
@@ -25,18 +27,19 @@ function isAvailable(servers)
         if(region.region == 'europe')
         {
           var available = false;
+          var hardware = region.hardware;
           for(var datacenterNb in region.datacenters)
           {
             var datacenter = region.datacenters[datacenterNb];
             if(datacenter.availability != 'unavailable' && datacenter.datacenter != 'default')
             {
-              serverAvailable(datacenter, server);
+              serverAvailable(datacenter, hardware);
               available = true;
             }
           }
           if(!available)
           {
-              console.log('Server "' + server + '" is not available');
+              console.log('Server "' + hardware + '" is not available');
           }
         }
       }
@@ -47,5 +50,13 @@ function isAvailable(servers)
 function serverAvailable(datacenter, server)
 {
   console.log('The server ' + server + ' is available in datacenter ' + datacenter.datacenter + ' with code ' + datacenter.availability);
-  clearInterval(interval);
+  const index = _servers.indexOf(server);
+  if (index > -1) {
+    _servers.splice(index, 1);
+  }
+  if(_servers.length < 1) {
+    clearInterval(interval);
+  }
+
+  console.log(_servers);
 }
