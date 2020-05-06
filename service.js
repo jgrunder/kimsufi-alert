b181aa52971de272c0fa77cdbedca8c434354e9f
Jonathan Grunder <jonathan.grunder@comdatagroup.fr>
2020-05-06 14:25:16 +0200

    Set interval to 60s

--- service.js
+++ service.js
@@ -24,7 +24,7 @@ if(Object.keys(_servers).length < 1) {
 // Launch the main function one time at start
 isAvailable(_servers);
 // Set interval, we call the function while there is at least one server to monitor
-var interval = setInterval(isAvailable, 10000, _servers);
+var interval = setInterval(isAvailable, 60000, _servers);
 
 /**
  * This function is the main function of the programm, it is called
